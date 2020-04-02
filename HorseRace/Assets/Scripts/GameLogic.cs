0ef93c601cd4edf91f1189745062db0c75c29570
Forve <tobias.forve@gmx.de>
2020-04-02 16:06:17 +0200

    safe

--- HorseRace/Assets/Scripts/GameLogic.cs
+++ HorseRace/Assets/Scripts/GameLogic.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
+using UnityEngine;
 
 public enum CardColor { HEART, SPADE, CLOVER, DIAMOND };
 public enum Values { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }; // delete Jack later
@@ -23,19 +23,19 @@ public class GameLogic : MonoBehaviour
 
     [Header("Track cards")]
     [SerializeField]
-    private GameObject[] cardsOfRacetrackPos;           // manage position of track cards
-    private List<string> cardsOfRacetrack;              // List of cards on track to play with
+    private GameObject[] cardsOfRacetrackPos;                       // manage position of track cards
+    private List<string> cardsOfRacetrack = new List<string>();     // List of cards on track to play with
 
     [Header("Horses")]
     public GameObject[] horses;
     public GameObject[] horsePos;
 
     [Header("Deck")]
-    [HideInInspector]
     public List<string> deck;
-    public Transform deckPile;                          // deck 
-    public Transform discardPile;                       // for the card drawn
-    // public GameObject[] deckPrefabs;                 // for anreachable prefabs
+    public List<string> discardPile = new List<string>();   // pile to save every drawn card to swap later with deckpile so we can start from begining. dont forget to shuffle again
+    public Transform deckPilePos;                           // deck 
+    public Transform discardPilePos;                        // for the card drawn
+    // public GameObject[] deckPrefabs;                     // for anreachable prefabs
 
     public void Start()
     {
@@ -46,7 +46,7 @@ public class GameLogic : MonoBehaviour
         // instantiate deckpile
         DealCards();
         // instantiate jacks
-        StartCoroutine( SetHorses());
+        StartCoroutine( DealHorses());
         
         // place rest on pile
     }
@@ -79,51 +79,26 @@ public class GameLogic : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// handout the cards 
-    /// </summary>
+    /// <summary>handout the cards </summary>
     void DealCards()
     {
         float zOffset = 0.03f;
         foreach (string card in deck)
         {
-            GameObject newCard = Instantiate(cardPrefab, new Vector3(deckPile.position.x, deckPile.position.y, deckPile.position.z + zOffset), Quaternion.identity, deckPile);
+            GameObject newCard = Instantiate(cardPrefab, new Vector3(deckPilePos.position.x, deckPilePos.position.y, deckPilePos.position.z + zOffset), Quaternion.identity, deckPilePos);
             zOffset += 0.03f;
             newCard.name = card;
         }
 
         // place last 9 cards of deck on top 9 pos and remove from deck
-        StartCoroutine( GetTrackCards());
+        StartCoroutine(DeadlTrackCards());
     }
 
-    /// <summary>
-    /// Instantiate Horses on their correct pos.
-    /// </summary>
-    IEnumerator SetHorses()
+    /// <summary> Instantiate the Cards on the Track & remove them from deck </summary>
+    IEnumerator DeadlTrackCards()
     {
-        int i = 0;
-
-        if (horses.Equals(null))
-        {
-            // exception handling!
-            
-        }
-
-        foreach (GameObject horse in horses)
-        {
-            yield return new WaitForSeconds(0.15f);
-            GameObject newHorse = Instantiate(horses[i], new Vector3(horsePos[i].transform.position.x, horsePos[i].transform.position.y, horsePos[i].transform.position.z), Quaternion.identity, horsePos[i].transform);
-            i++;
-        }
-    }
-
-    /// <summary>
-    /// Get the track cards to play with
-    /// </summary>
-    IEnumerator GetTrackCards()
-    {
-        cardsOfRacetrack = new List<string>();
-
+        discardPile.Clear();
+        //add last 9 cards to track cards and remove them from deck
         for (int i = 0; i < 9; i++)
         {
             cardsOfRacetrack.Add(deck.Last<string>());
@@ -143,6 +118,75 @@ public class GameLogic : MonoBehaviour
         //{
         //    print(c);
         //}
+    }
+
+    /// <summary> Instantiate Horses on their correct pos.</summary>
+    IEnumerator DealHorses()
+    {
+        int i = 0;
+
+        if (horses.Equals(null))
+        {
+            // exception handling!
+
+        }
+
+        foreach (GameObject horse in horses)
+        {
+            yield return new WaitForSeconds(0.15f);
+            GameObject newHorse = Instantiate(horses[i], new Vector3(horsePos[i].transform.position.x, horsePos[i].transform.position.y, horsePos[i].transform.position.z), Quaternion.identity, horsePos[i].transform);
+            i++;
+        }
+    }
+
+    /// <summary> Restack deck with the cards in the discardPile </summary>
+    void RestackDeck()
+    {
+        foreach (Transform child in discardPilePos)
+        {
+            if (child.CompareTag("Card"))
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        deck.Clear();
+
+        foreach (string card in discardPile)
+        {
+            deck.Add(card);
+        }
+        discardPile.Clear();
+        Shuffle(deck);
+    }
+
+    // ---------------- Playing -----------------
+
+    float zOffset = 0f;
+    public int cardsDrawn = 0;
+
+    public void PlayDeckCard()
+    {
+        // move from deck to discardPile
+        discardPile.Add(deck.Last<string>());
+
+        //instantiate last drawn card
+        string card = deck.Last<string>();
+        GameObject drawnCard = Instantiate(cardPrefab, new Vector3(discardPilePos.transform.position.x, discardPilePos.transform.position.y, discardPilePos.transform.position.z - zOffset), Quaternion.identity, discardPilePos.transform);
+        drawnCard.name = card;
+        drawnCard.GetComponent<UpdateSprite>().faceUp = true;
+        zOffset += 0.03f;
+
+        cardsDrawn++;    
+        //remove from deck
+        deck.RemoveAt(deck.Count - 1);
+
+        // if deck is empty copy all discardPile cards to deck and clear discardPile
+        if (cardsDrawn >= 39)
+        {
+            cardsDrawn = 0;
+            RestackDeck();
+        }
     }
 }
 
---
+++ HorseRace/Assets/Scripts/InputController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    int count = 0;
+
+    private GameLogic gameLogic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameLogic = FindObjectOfType<GameLogic>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetMouseClick();
+    }
+
+    void GetMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10));
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                // what has been hit? Deck/Card/EmptySlot...
+                if (hit.collider.CompareTag("Deck"))
+                {
+                    DeckClicked();
+                }
+                else if (hit.collider.CompareTag("Horse"))
+                {
+                    HorseClicked(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    private void DeckClicked()
+    {
+        gameLogic.PlayDeckCard();
+        // get back suite of card
+            // move horse 
+    }
+
+    private void HorseClicked(GameObject gameObject)
+    {
+        
+    }
+
+    void HorseClick(HorseControll horse)
+    {
+        
+    }
+
+}
