d0c227dcb048820bb66c7a10b38f24e03d67e632
Forve <tobias.forve@gmx.de>
2020-04-13 18:36:06 +0200

    up

---
+++ HorseRace/Assets/Scripts/Menu/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private static PlayerController _instance;
+
+    public static PlayerController Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    //------------
+
+    public string name;
+    public GameObject inputField;
+}
